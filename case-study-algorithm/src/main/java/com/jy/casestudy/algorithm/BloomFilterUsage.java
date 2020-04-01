5d16768ea4b9512810ed0983326bc55682889f95
yangjian <yangjian@ishansong.com>
2020-04-01 18:30:38 +0800

    布隆过滤案例优化

--- case-study-algorithm/src/main/java/com/jy/casestudy/algorithm/BloomFilterUsage.java
+++ case-study-algorithm/src/main/java/com/jy/casestudy/algorithm/BloomFilterUsage.java
@@ -12,11 +12,11 @@ public class BloomFilterUsage {
         for (int i = 0; i < 10000000; i++) {
             filter.put(i);
         }
-        Assert.isTrue(filter.mightContain(1), "不包含命中数据");
-        Assert.isTrue(filter.mightContain(2), "不包含命中数据");
-        Assert.isTrue(filter.mightContain(3), "不包含命中数据");
+        Assert.isTrue(filter.mightContain(1), "不包含1");
+        Assert.isTrue(filter.mightContain(2), "不包含2");
+        Assert.isTrue(filter.mightContain(3), "不包含3");
         //集合中未放置该数据应该报错
-        Assert.isTrue(filter.mightContain(20000000), "不包含命中数据");
+        Assert.isTrue(filter.mightContain(20000000), "不包含20000000");
         long end = System.currentTimeMillis();
         System.out.println("执行时间：" + (end - start));
     }
